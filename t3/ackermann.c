6707e17f8daadadd13ad3a0ce4ad4eda7dc729af
Jarota <32527884+Jarota@users.noreply.github.com>
2018-10-18 11:03:58 +0100

    q3 code done

--- t3/ackermann.c
+++ t3/ackermann.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#define numRegisterWindows 6
+#include <time.h>
+#define numRegisterWindows 16
 
 int calls;
 int depth;
@@ -34,25 +35,51 @@ void underflowCheck(){
 	}
 }
 
-int ackermann(int x, int y){
+int ackermannWithChecks(int x, int y){
 	overflowCheck();
 	int ret;
 	if(x==0){
 		ret = y + 1;
 	}
 	else if(y==0){
-		ret = ackermann(x-1, 1);
+		ret = ackermannWithChecks(x-1, 1);
 	}
 	else {
-		ret = ackermann(x-1, ackermann(x, y-1));
+		ret = ackermannWithChecks(x-1, ackermannWithChecks(x, y-1));
 	}
 	underflowCheck();
 	return ret;
 }
 
+int ackermann(int x, int y){
+	if(x==0){
+		return y + 1;
+	}
+	else if(y==0){
+		return ackermann(x-1, 1);
+	}
+	else {
+		return ackermann(x-1, ackermann(x, y-1));
+	}
+}
+
 
 int main(int argc, char** argv){
-	ackermann(3, 6);
-	printf("Number of Calls: %d\nMaximum Register Window Depth: %d\nOverflows: %d\nUnderflows: %d\n", calls, maxDepth, overflows, underflows);
+	//ackermannWithChecks(3, 6);
+	//printf("Number of Calls: %d\nMaximum Register Window Depth: %d\nOverflows: %d\nUnderflows: %d\n", calls, maxDepth, overflows, underflows);
+	int start, end;
+	volatile int x = 3;	// prevent compiler optimising away the superfluous calls to ackermann
+	int i = 0;
+	start = clock();	// start time
+	while(i<100000){
+		ackermann(x, 6);
+		i++;
+	}
+	end = clock();		// end time
+
+	double timeTaken = (double) end - start;
+	double timePerCall = timeTaken / 100000;
+	printf("Time Taken: %.1f\nAverage Time Per Call: %.3f", timeTaken, timePerCall);
+
 	return 0;
 }
